4a7248267d32697430f481ac6b7591e9e9399a61
unknown <jabaraxd@gmail.com>
2024-12-14 15:29:05 -0400

    first commit

---
+++ MyFirstVueApp/src/components/MyApp01.vue
@@ -0,0 +1,21 @@
+<script> 
+
+</script>
+
+<template>
+     <div> Mi primera app en <span>Vue.js</span> </div>
+</template>
+
+<style scoped>
+     div {
+
+     color: #35495f;    
+     font-size: 1.6rem;       
+     }
+     span {
+          color: #40902f
+     }
+
+     
+</style>
+
